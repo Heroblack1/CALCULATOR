2e4df94cd86cad14fc2371e5c77ea60a86a6fd4b
Heroblack1 <danjumahero@gmail.com>
2024-07-21 16:24:39 +0100

    calc

---
+++ calc.js
@@ -0,0 +1,78 @@
+function one () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '1';
+}
+function two () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '2';
+}
+function three () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '3';
+}
+function four () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '4';
+}
+function five () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '5';
+}
+function six () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '6';
+}
+function seven () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '7';
+}
+function eight () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '8';
+}
+function nine () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '9';
+}
+function zer () {   
+    let input = document.getElementById('input');
+    input.value = input.value + '0';
+}
+function cool () {   
+    let input = document.getElementById('input');
+    input.value = 'One cool motherfucker';
+}
+function clearScreen () {   
+    let input = document.getElementById('input');
+    input.value = '';
+}
+function divid () {   
+    let input = document.getElementById('input');
+    input.value += '/';
+}
+function eqss () {   
+    let input = document.getElementById('input');
+    input.value = eval(input.value);
+}
+function dot () {   
+    let input = document.getElementById('input');
+    input.value += '.';
+}
+function multip () {   
+    let input = document.getElementById('input');
+    input.value += '*';
+}
+function minus () {   
+    let input = document.getElementById('input');
+    input.value += '-';
+}
+function plus () {   
+    let input = document.getElementById('input');
+    input.value += '+';
+}
+function delet () {   
+    let input = document.getElementById('input');
+    input.value = input.value.toString().slice(0, -1)
+}
+
+
